afec1e78e4f9d70d6d22500214c88585d529a367
Jeremiah Shirk <jshirk@gmail.com>
2012-10-11 05:32:46 -0500

    Support subaccounts API

--- lib/monitis-SDK/user.rb
+++ lib/monitis-SDK/user.rb
@@ -45,5 +45,68 @@ class User < MonitisClient
     # get 'authToken', {secretkey: secretkey} { |x| x['authToken'] }
     new_auth_token
   end
+end
 
+# Manage User's sub accounts
+class SubAccounts < MonitisClient
+
+  # Add a new sub account
+  #
+  # === Required arguments
+  # * first - First name of the sub account owner
+  # * last - last name of the sub account owner
+  # * email - email address to be used as user name for the sub account
+  # * password - password of the sub account
+  # * group - group of the sub account
+  def add(first, last, email, password, group)
+    args = {firstName: first, lastName: last, email: email,
+            password: password, group:group}
+    post('addSubAccount', args)
+  end
+
+  # Add new pages for the specified sub account
+  #
+  # === Required arguments
+  # * id - user id of the specified subaccount to add pages to
+  # * pages - Pages names to share with sub account, as an array
+  #   or semicolon separated string
+  #
+  # === Optional arguments
+  def add_pages(id, pages)  
+    pages = pages.join(',') if pages.class == Array
+    args = {userId: id, pageNames: pages}
+    post('addPagesToSubAccount', args)
+  end
+
+  # Delete a sub account
+  #
+  # === Required arguments
+  # * id - User id of the sub account to delete
+  def delete(id)
+    post('deleteSubAccount', userId: id)
+  end
+
+  # Delete pages from the specified sub account
+  #
+  # === Required arguments
+  # * id - user ID of the sub account to delete pages from
+  # * pages - Pages names to remove from the sub account, as an array
+  #   or semicolon separated string
+  #
+  # === Optional arguments
+  def delete_pages(id, pages)
+    pages = pages.join(',') if pages.class == Array
+    args = {userId: id, pageNames: pages}
+    post('deletePagesFromSubAccount', args)
+  end
+
+  # Get a list of a user's sub accounts
+  def subaccounts()
+    get('subAccounts')
+  end
+
+  # Get all pages for a user's sub accounts
+  def pages()
+    get('subAccountPages')
+  end
 end
---
+++ spec/subaccounts_spec.rb
@@ -0,0 +1,60 @@
+require 'monitis-SDK'
+require 'securerandom'
+
+def temp_name()
+  "test_#{SecureRandom.hex 4}"
+end
+
+describe Agents do
+  before :each do
+    @sub = SubAccounts.new(use_production: true)
+    @layout = Layout.new(use_production: true)
+    temp = temp_name
+    
+    # add a test sub account
+    acct = @sub.add('Test', 'User', "#{temp}@test.com", temp, temp)
+    @acct_id = acct['data']['userId']
+    @temp_accts = [@acct_id]
+    
+    # add a page to the subaccount
+    @page_name = @layout.pages.first['title']
+    @sub.add_pages(@acct_id, @page_name)
+  end
+
+  after :each do
+    # delete test subaccounts
+    @temp_accts.each {|id| @sub.delete(id)}
+  end
+
+  it 'should add a sub account' do
+    temp = temp_name
+    result = @sub.add('Test', 'User', "#{temp}@test.com", temp, temp)
+    result['status'].should == 'ok'
+    @temp_accts << result['data']['userId']
+  end
+
+  it 'should add a page to a sub account' do
+    result = @sub.add_pages(@acct_id, @layout.pages.first['title'])
+    result.status.should == 'ok'
+  end
+
+  it 'should delete a sub account' do
+    result = @sub.delete(@temp_accts.pop)
+    result.status.should == 'ok'
+  end
+
+  it 'should delete a page from a sub account' do
+    result = @sub.delete_pages(@acct_id, @page_name)
+    result.status.should == 'ok'
+  end
+
+  it 'should get a list of all sub accounts' do
+    result = @sub.subaccounts
+    result.keep_if {|x| x['id'] == @acct_id}.length.should > 0
+  end
+
+  it 'should get a list of all sub accounts pages' do
+    result = @sub.pages
+    result.keep_if {|x| x['id'] == @acct_id}.length.should > 0
+  end
+end
